71c91805ce1487070cad6f08ce0fc1e8107cc288
SwaranjalSingh <117919022+SwaranjalSingh@users.noreply.github.com>
2023-02-20 17:06:53 +0530

    added --> force_destroy = true

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "SwaranjalBucket" {
   bucket = "swarajal-bucket"
-
+  force_destroy = true
   tags = {
     "Name" = "Swaranjal"
     "Owner" = "swaranjal.singh@cloudeq.com"
